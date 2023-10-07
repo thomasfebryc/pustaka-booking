e7deb8e307d3eb04303e63bc00a9b8f3de1b1ddd
thomasfebryc <146792394+thomasfebryc@users.noreply.github.com>
2023-10-07 13:08:27 +0700

    Pertemuan 1&2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>"; 
+        echo "Nama saya Thomas febry cahyono
+            Saya tingga di daerah beji
+            olah raga yang saya sukai adalah
+            futsal";
+    }
+}
